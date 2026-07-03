7ded754be69cb834a6e9e83a86f2d701c0e2ba3f
Yuri Shimoki <yshimoki@protonmail.com>
2026-07-03 17:06:20 -0300

    LoadFromBinary() improved failure checks.

--- src/Serialize.hpp
+++ src/Serialize.hpp
@@ -19,17 +19,15 @@ namespace srl
     auto LoadFromBinary(const std::filesystem::path& filePath) -> std::expected<T, IoError>
     {
         auto file = std::ifstream(filePath, std::ios::binary);
-
         if (!file.is_open())
         {
             return std::unexpected{ IoError::OpenError };
         }
 
-        // Initialize enough space for T without constructing it.
         auto loadDestination = T();
-        file.read(&loadDestination, sizeof(T));
+        file.read(reinterpret_cast<char*>(&loadDestination), sizeof(T));
 
-        if (auto bytesRead = file.gcount(); bytesRead != sizeof(T))
+        if (file.fail() || file.gcount() != sizeof(T))
         {
             return std::unexpected{ IoError::ReadError };
         }

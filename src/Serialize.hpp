ca2f9649568f3a7a234a53ea952fe7ea0c2a4b88
Yuri Shimoki <yshimoki@protonmail.com>
2026-07-03 16:56:57 -0300

    LoadFromBinary() v1 finished.

--- src/Serialize.hpp
+++ src/Serialize.hpp
@@ -3,19 +3,37 @@
 #include <fstream>
 #include <filesystem>
 #include <expected>
+#include <cstddef>
 
 namespace srl
 {
     enum class IoError
     {
+        None,
         OpenError,
         WriteError,
         ReadError,
     };
 
     template<typename T>
-    auto LoadFromBinary(const std::filesystem::path& file) -> std::expected<T, IoError>
+    auto LoadFromBinary(const std::filesystem::path& filePath) -> std::expected<T, IoError>
     {
-        
+        auto file = std::ifstream(filePath, std::ios::binary);
+
+        if (!file.is_open())
+        {
+            return std::unexpected{ IoError::OpenError };
+        }
+
+        // Initialize enough space for T without constructing it.
+        auto loadDestination = T();
+        file.read(&loadDestination, sizeof(T));
+
+        if (auto bytesRead = file.gcount(); bytesRead != sizeof(T))
+        {
+            return std::unexpected{ IoError::ReadError };
+        }
+
+        return loadDestination;
     }
 }

b18c9bdf1a1121ec36ef269143b4479671cef70f
Yuri Shimoki <yshimoki@protonmail.com>
2026-07-23 13:57:39 -0300

    WIP: Add reflection

--- src/core.hpp
+++ src/core.hpp
@@ -45,3 +45,57 @@ namespace srl
 		return deserializedObject;
 	}
 }
+
+// Biblioteca Serialize
+namespace srl
+{
+	//template<typename T>
+	//struct MemberPointerTraits
+	//{
+	//};
+
+	//template<typename M, typename C>
+	//struct MemberPointerTraits<M C::*>
+	//{
+	//	using OwnerType = C;
+	//	using MemberType = M;
+	//};
+
+	//template<auto T>
+	//using PointedType = typename MemberPointerTraits<decltype(T)>::MemberType;
+
+	template <typename T>
+	constexpr auto kSerializableFields = std::tuple{};
+
+	template<typename T, typename... Ms>
+	consteval auto ReflectFields(Ms T::*... memberPointers)
+	{
+		return std::make_tuple(memberPointers...);
+	}
+
+	template<typename ClassType, typename MemberType>
+	void SerializeField(const ClassType& object, MemberType ClassType::* memberPointer)
+	{
+		const auto objectBaseAddress = reinterpret_cast<const std::byte*>(&object);
+		const auto objectMemberAddress = reinterpret_cast<const std::byte*>(&(object.*memberPointer));
+
+		const     auto memberOffset = objectMemberAddress - objectBaseAddress;
+		constexpr auto memberSize = sizeof(MemberType);
+
+		// [TODO]: Write member serialization logic
+	}
+
+	template<typename T>
+	auto SerializeToBinary(const T& object) -> BinarySerialization<T>
+	{
+		// Serializable fields are passed in as member pointers
+		const auto memberPointers = kSerializableFields<T>;
+
+		std::apply(
+			[&](auto... memberPointers) {
+				(SerializeField(object, memberPointers), ...);
+			},
+			memberPointers
+		);
+	}
+}
